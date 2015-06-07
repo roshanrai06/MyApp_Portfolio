c0ed3b4abf6c3ede831fb5d3d2653748764008d0
Roshan Rai <roshanrai06@gmail.com>
2015-06-07 17:22:42 +0530

    Final commit

--- app/src/main/java/com/nanodegree/roshan/myapp_portfolio/MainActivity.java
+++ app/src/main/java/com/nanodegree/roshan/myapp_portfolio/MainActivity.java
@@ -1,6 +1,5 @@
 package com.nanodegree.roshan.myapp_portfolio;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
